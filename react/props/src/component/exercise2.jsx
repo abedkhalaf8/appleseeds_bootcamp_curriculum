116a540ff71c327c111563f3c2296d1b124d8c9e
Abed Khalaf <kfabdall@gmail.com>
2022-05-16 12:25:48 +0300

    Update exercise2.jsx

---
+++ react/props/src/component/exercise2.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class Exercise1 extends React.Component {
+  render() {
+    return (
+       <div ><a href="https://google.com">click me</a></div>
+    );
+  }
+}

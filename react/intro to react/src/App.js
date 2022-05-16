349333d912f26557269a6c22873a33dcea13f97e
Abed Khalaf <100962928+abedkhalaf8@users.noreply.github.com>
2022-05-16 12:02:28 +0300

    intro to react exercises

---
+++ react/intro to react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import {Exercise2} from './components/exercise2';
+
+function App() {
+  return (
+      <div
+           {<Exercise2/>}
+      </div>
+  )
+}
+export default App;
+

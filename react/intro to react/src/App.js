3a616a55087c3ad1ba1fa8834110c999ca708757
Abed Khalaf <kfabdall@gmail.com>
2022-05-16 12:04:41 +0300

    Update App.js

--- react/intro to react/src/App.js
+++ react/intro to react/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
+import {Exercise1} from './components/exercise1';
 import {Exercise2} from './components/exercise2';
+import {Exercise3} from './components/exercise3';
+import {Exercise4} from './components/exercise4';
 
 function App() {
   return (
-      <div
+      <div>
+           {<Exercise1/>}
            {<Exercise2/>}
+           {<Exercise3/>}
+           {<Exercise4/>}
       </div>
   )
 }

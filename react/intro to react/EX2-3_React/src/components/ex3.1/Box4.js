9b033cce167a7c4b04694e74fe3c0f4dcb583403
Abed Khalaf <100962928+abedkhalaf8@users.noreply.github.com>
2022-05-17 13:08:51 +0300

    Delete react directory

--- react/intro to react/EX2-3_React/src/components/ex3.1/Box4.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './style.css';
-function Box4() {
-  return <div className='box4 box'></div>;
-}
-
-export default Box4;
--- react/intro to react/src/components/exercise2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export class Exercise1 extends React.Component {
-  render() {
-    return (
-       <div ><a href="https://google.com">click me</a></div>
-    );
-  }
-}
